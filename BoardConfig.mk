46e71d3a33a8f79b4c5522c0c8a96bf23d4b64ff
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-04-23 18:51:15 +0900

    Use common kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,8 +19,8 @@ BOARD_KERNEL_CMDLINE := console=ttyDCC0 androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x00200000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_FORCE_RAMDISK_ADDRESS := 0x1300000
-TARGET_KERNEL_CONFIG := msm7630-perf_defconfig
-TARGET_KERNEL_SOURCE := kernel/kyocera/isw11k
+TARGET_KERNEL_CONFIG := kyocera-isw11k_defconfig
+TARGET_KERNEL_SOURCE := kernel/kyocera/msm7630
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
